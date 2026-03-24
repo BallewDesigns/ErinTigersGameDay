14b63117a7b304007b33dfde9f8b863821fe968d
BallewDesigns <robin@ballewdesignsllc.com>
2026-03-24 17:19:21 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE = 'gameday-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'gameday-v2';
+const ASSETS = [
+  '/ErinTigersGameDay/',
+  '/ErinTigersGameDay/index.html',
+  '/ErinTigersGameDay/manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
